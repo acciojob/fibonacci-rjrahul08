7b28527162744de742c877134b178645c5f5ebf2
Rahul Prasad Shaw <61372250+rjrahul08@users.noreply.github.com>
2023-07-19 13:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-// your code here
+  // Create an array to store calculated Fibonacci numbers
+  const fibArray = [0, 1];
+
+  // Calculate the nth Fibonacci number using dynamic programming (Memoization)
+  for (let i = 2; i <= num; i++) {
+    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+  }
+
+  // Return the nth Fibonacci number
+  return fibArray[num];
 }
 
 module.exports = fibonacci;
